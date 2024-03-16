2c21d4c826949a5a85b607ba00fa0fdf4056213a
ghritak <ghritakjyotikalita@gmail.com>
2024-03-17 01:20:38 +0530

    get link data

--- constants/linkData.js
+++ constants/linkData.js
@@ -1,26 +1,37 @@
 export const linkData = [
   {
+    id: 1,
     name: 'LinkedIn',
     logo: 'http://localhost:4000/images/logo/linkedin.png',
   },
   {
+    id: 2,
     name: 'Instagram',
     logo: 'http://localhost:4000/images/logo/instagram.png',
   },
   {
+    id: 3,
     name: 'Facebook',
     logo: 'http://localhost:4000/images/logo/facebook.png',
   },
   {
+    id: 4,
     name: 'Twitter',
     logo: 'http://localhost:4000/images/logo/twitter.png',
   },
   {
+    id: 5,
     name: 'Portfolio',
     logo: 'http://localhost:4000/images/logo/portfolio.png',
   },
   {
+    id: 6,
     name: 'GitHub',
     logo: 'http://localhost:4000/images/logo/github.png',
+  },
+  {
+    id: 7,
+    name: 'OnlyFans',
+    logo: 'http://localhost:4000/images/logo/onlyfans.png',
   },
 ];
