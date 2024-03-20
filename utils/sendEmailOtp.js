7d9f621591e039e6db41787a860a14de497cab82
ghritak <ghritakjyotikalita@gmail.com>
2024-03-20 23:47:57 +0530

    e

--- utils/sendEmailOtp.js
+++ utils/sendEmailOtp.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
   },
 });
 
