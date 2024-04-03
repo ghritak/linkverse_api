6c099a3f069bc7056286e28a9ee4393962d65fa3
ghritak <ghritakjyotikalita@gmail.com>
2024-04-03 12:48:48 +0530

    try catch getuser

--- routes/links/getUser.js
+++ routes/links/getUser.js
@@ -1,25 +1,36 @@
 const getUser = async (req, res) => {
-  const username = req.query.username;
-  if (!username)
-    return res.status(400).json({ message: 'Please provide a user name.' });
-  const linkCollection = req.database.collection('links');
-  const userCollection = req.database.collection('users');
-  const link = await linkCollection.findOne({ username }, { maxTimeMS: 15000 });
-  const user = await userCollection.findOne({ username }, { maxTimeMS: 15000 });
-  if (!link || !user) {
-    return res
-      .status(400)
-      .json({ message: "Couldn't found user with the given username." });
+  try {
+    const username = req.query.username;
+    if (!username)
+      return res.status(400).json({ message: 'Please provide a user name.' });
+    const linkCollection = req.database.collection('links');
+    const userCollection = req.database.collection('users');
+    const link = await linkCollection.findOne(
+      { username },
+      { maxTimeMS: 15000 }
+    );
+    const user = await userCollection.findOne(
+      { username },
+      { maxTimeMS: 15000 }
+    );
+    if (!link || !user) {
+      return res
+        .status(400)
+        .json({ message: "Couldn't found user with the given username." });
+    }
+
+    user && delete user.password;
+    user && delete user.email;
+
+    const data = {
+      ...link,
+      ...user,
+    };
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
-
-  user && delete user.password;
-  user && delete user.email;
-
-  const data = {
-    ...link,
-    ...user,
-  };
-  return res.status(200).json(data);
 };
 
 export default getUser;
--- routes/links/getUserProfile.js
+++ routes/links/getUserProfile.js
@@ -1,29 +1,40 @@
 const getUserProfile = async (req, res) => {
-  const username = req.query.username;
-  if (!username)
-    return res.status(400).json({ message: 'Please provide a user name.' });
-  const linkCollection = req.database.collection('links');
-  const userCollection = req.database.collection('users');
-  const link = await linkCollection.findOne({ username }, { maxTimeMS: 15000 });
-  const user = await userCollection.findOne({ username }, { maxTimeMS: 15000 });
+  try {
+    const username = req.query.username;
+    if (!username)
+      return res.status(400).json({ message: 'Please provide a user name.' });
+    const linkCollection = req.database.collection('links');
+    const userCollection = req.database.collection('users');
+    const link = await linkCollection.findOne(
+      { username },
+      { maxTimeMS: 15000 }
+    );
+    const user = await userCollection.findOne(
+      { username },
+      { maxTimeMS: 15000 }
+    );
 
-  if (!link || !user) {
-    return res
-      .status(400)
-      .json({ message: "Couldn't found user with the given username." });
+    if (!link || !user) {
+      return res
+        .status(400)
+        .json({ message: "Couldn't found user with the given username." });
+    }
+
+    if (user._id.toString() !== req.user_id) {
+      return res.status(401).json({ message: 'Unauthorized token.' });
+    }
+
+    user.password && delete user.password;
+
+    const data = {
+      ...link,
+      ...user,
+    };
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
-
-  if (user._id.toString() !== req.user_id) {
-    return res.status(401).json({ message: 'Unauthorized token.' });
-  }
-
-  user.password && delete user.password;
-
-  const data = {
-    ...link,
-    ...user,
-  };
-  return res.status(200).json(data);
 };
 
 export default getUserProfile;
