13b70bd4bfc8fdc1e11aa32a96d740ef6a519ba5
ghritak <ghritakjyotikalita@gmail.com>
2024-05-04 13:22:31 +0530

    status created

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -33,7 +33,7 @@ const signup = async (req, res) => {
       links: [],
     });
 
-    return res.status(200).json({ message: 'User created successfully' });
+    return res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal server error' });
