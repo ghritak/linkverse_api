15eb4c76e03a71a0477c836a9c03461d0197fe0c
ghritak <ghritakjyotikalita@gmail.com>
2024-05-10 03:36:23 +0530

    static logos

--- constants/linkData.js
+++ constants/linkData.js
@@ -2,46 +2,46 @@ export const linkData = [
   {
     id: 1,
     name: 'LinkedIn',
-    logo: 'http://localhost:4000/images/logo/linkedin.png',
+    logo: 'https://linkverse.onrender.com/images/logo/linkedin.png',
   },
   {
     id: 2,
     name: 'Instagram',
-    logo: 'http://localhost:4000/images/logo/instagram.png',
+    logo: 'https://linkverse.onrender.com/images/logo/instagram.png',
   },
   {
     id: 3,
     name: 'Facebook',
-    logo: 'http://localhost:4000/images/logo/facebook.png',
+    logo: 'https://linkverse.onrender.com/images/logo/facebook.png',
   },
   {
     id: 4,
     name: 'Twitter',
-    logo: 'http://localhost:4000/images/logo/twitter.png',
+    logo: 'https://linkverse.onrender.com/images/logo/twitter.png',
   },
   {
     id: 5,
     name: 'Portfolio',
-    logo: 'http://localhost:4000/images/logo/portfolio.png',
+    logo: 'https://linkverse.onrender.com/images/logo/portfolio.png',
   },
   {
     id: 6,
     name: 'GitHub',
-    logo: 'http://localhost:4000/images/logo/github.png',
+    logo: 'https://linkverse.onrender.com/images/logo/github.png',
   },
   {
     id: 7,
     name: 'YouTube',
-    logo: 'http://localhost:4000/images/logo/youtube.png',
+    logo: 'https://linkverse.onrender.com/images/logo/youtube.png',
   },
   {
     id: 9,
     name: 'Threads',
-    logo: 'http://localhost:4000/images/logo/threads.png',
+    logo: 'https://linkverse.onrender.com/images/logo/threads.png',
   },
   {
     id: 10,
     name: 'OnlyFans',
-    logo: 'http://localhost:4000/images/logo/onlyfans.png',
+    logo: 'https://linkverse.onrender.com/images/logo/onlyfans.png',
   },
 ];
