412a2cc1bfefc09ca2516a3e942ef3958f40c7b5
ghritak <ghritakjyotikalita@gmail.com>
2024-05-29 23:44:21 +0530

    remove cronning

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import cors from 'cors';
 import session from 'express-session';
-import './cronning.js';
+// import './cronning.js';
 
 dotenv.config();
 
